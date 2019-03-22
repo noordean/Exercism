65dbe4aeff2c33f5a0b3281847086231dea2fb74
noordean <ebroyeem90@gmail.com>
2019-03-22 23:16:31 +0100

    convert charlist to string, split and iterate through

--- elixir/rna-transcription/rna_transcription.exs
+++ elixir/rna-transcription/rna_transcription.exs
@@ -9,5 +9,20 @@ defmodule RNATranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
+    dna
+      |> to_string
+      |> String.split("")
+      |> Enum.map(&dna_to_rna_mapping[&1])
+      |> Enum.join("")
+      |> to_charlist
+  end
+
+  def dna_to_rna_mapping do
+  %{
+      "G" => "C",
+      "C" => "G",
+      "T" => "A",
+      "A" => "U"
+    }
   end
 end
