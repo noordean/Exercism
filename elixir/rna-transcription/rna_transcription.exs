7811e89a2d679e3d903cf960286fc85e26e6923d
noordean <ebroyeem90@gmail.com>
2019-03-24 11:25:22 +0100

    use ? for getting letter's codepoint: e.g ?G = 71

--- elixir/rna-transcription/rna_transcription.exs
+++ elixir/rna-transcription/rna_transcription.exs
@@ -9,20 +9,15 @@ defmodule RNATranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    dna
-      |> to_string
-      |> String.split("")
-      |> Enum.map(&dna_to_rna_mapping[&1])
-      |> Enum.join("")
-      |> to_charlist
+    dna |> Enum.map(&dna_to_rna_mapping[&1])
   end
 
   def dna_to_rna_mapping do
-  %{
-      "G" => "C",
-      "C" => "G",
-      "T" => "A",
-      "A" => "U"
-    }
+    %{
+        ?G => ?C,
+        ?C => ?G,
+        ?T => ?A,
+        ?A => ?U
+      }
   end
 end
