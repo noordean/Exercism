489b4996288ba11649491ec3df71235bb6535f25
noordean <ebroyeem90@gmail.com>
2019-03-27 09:03:28 +0100

    implement two-fer for JS

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,7 +1,1 @@
-export const isLeap = (num) => {
-  if ((num % 4 === 0 && num % 100 !== 0) || num % 400 === 0) {
-    return true;
-  }
-
-  return false;
-};
+export const isLeap = num => (num % 4 === 0 && num % 100 !== 0) || num % 400 === 0;
